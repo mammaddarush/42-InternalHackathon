3d22039c47ed33c5cc59d94b5e9bf5cb8852a3b2
IshtarRodrev <alinakechedzi@rambler.ru>
2025-10-21 00:21:38 +0000

    intial

---
+++ enum.php
@@ -0,0 +1,25 @@
+enum Title: string {
+    case Mastermind = 'Mastermind';
+    case Hacker = 'Hacker';
+    case Strategist = 'Strategist';
+    // Add more titles as needed
+}
+
+enum Status: string {
+    case Cadet = 'cadet at 42cursus';
+    case Alumni = 'alumni';
+    case Staff = 'staff';
+}
+
+enum Coalition: string {
+    case 0 = 'Apollo 42';
+    case 1 = 'Area 42';
+    case 2 = 'Magrathea';
+}
+
+enum Location: string {
+    case Heilbronn = 'Heilbronn';
+    case Berlin = 'Berlin';
+    case Paris = 'Wolfsburg';
+    case Paris = 'Paris';
+}
